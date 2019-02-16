8256ffd25d606a0a0bcea30fd849a3a8d062ea84
BuildTools <cwmulcah@mtu.edu>
2019-02-15 19:40:47 -0500

    Initial gradle setup commit

---
+++ src/test/java/server/ServerTest.java
@@ -0,0 +1,19 @@
+package server;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test()
+    {
+
+    }
+}
