9b2cd32cfe99d8702b8525889b1b59047fc03974
Alec Rospierski <asrospie@TRANS.local>
2019-03-20 09:34:33 -0400

    Added add and remove lobby functions that interact with the database

--- src/main/java/server/Database.java
+++ src/main/java/server/Database.java
@@ -30,4 +30,78 @@ public class Database {
 
         }
     }
+
+    /**
+     * addLobby
+     * calls the addLobby procedure from the database
+     * Parameters: code (the lobby code), uri (the playlist uri)
+     * Return: 0 (if failed), 1 (if completed)
+     */
+    public int addLobby(String code, String uri) {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        int rowcount;
+
+        try {
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+
+        try {
+            stmt = conn.prepareStatement(
+                "addLobby(?, ?)"
+            );
+            stmt.setString(code, uri);
+            rowcount = stmt.executeUpdate();
+            if (rowcount != 1) {
+                System.err.println("Something went wrong.");
+                conn.rollback();
+            } else {
+                conn.commit();
+            } 
+        } catch(SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+        return 1;
+    }
+    
+    /**
+     * removeLobby
+     * calls the removeLobby procedure from the database
+     * Parameters: code (the lobby code)
+     * Return: 0 (if failed), 1 (if completed)
+     */
+    public int removeLobby(String code) {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        int rowcount;
+
+        try {
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+
+        try {
+            stmt = conn.prepareStatement(
+                "removeLobby(?)"
+            );
+            stmt.setString(code);
+            rowcount = stmt.executeUpdate();
+            if (rowcount != 1) {
+                System.err.println("Something went wrong.");
+                conn.rollback();
+            } else {
+                conn.commit();
+            } 
+        } catch(SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+        return 1;
+    }
 }
